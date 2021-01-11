119a4ff734008d01584128b3c286c3f353808d3b
kris-lalev <kristian.lalevv@gmail.com>
2021-01-11 23:23:10 +0200

    Removed some commeted lines
    Removed some commeted lines

--- HuffmanAlgorithm/huffman.cpp
+++ HuffmanAlgorithm/huffman.cpp
@@ -1,38 +1,28 @@
 #include "huffman.h"
 
-#define COUNT 10  
+ 
 void print2DUtil(node_ptr root, int space)
 {
-	// Base case  
+	
 	if (root == NULL) {
 		for (int i = 0; i < space; i++)
 			cout << " ";
 		cout << "NULL" << "\n";
 		return;
 	}
-		
-
-	// Increase distance between levels  
-	space += COUNT;
-
-	// Process right child first  
+	space += 5;
 	print2DUtil(root->right, space);
-
-	// Print current node after space  
-	// count  
 	cout << endl;
-	for (int i = COUNT; i < space; i++)
-		cout << " ";
-	cout << root->id << "||" << root->code << "\n";
 
-	// Process left child  
+	for (int i = 5; i < space; i++)
+		cout << " ";
+
+	cout << root->id << "||" << root->code << "\n";
 	print2DUtil(root->left, space);
 }
 
-// Wrapper over print2DUtil()  
 void print2D(node_ptr root)
 {
-	// Pass initial space count as 0  
 	print2DUtil(root, 0);
 }
 
@@ -77,7 +67,7 @@ string huffman::decimal_to_binary(int in)
 		temp += ('0' + in % 2);
 		in /= 2;
 	}
-	result.append(8 - temp.size(), '0');													//append '0' ahead to let the result become fixed length of 8
+	result.append(8 - temp.size(), '0');
 	for (int i = temp.size() - 1; i >= 0; i--)
 	{
 		result += temp[i];
@@ -106,7 +96,6 @@ void huffman::create_pq()
 {
 	in_file.open(in_file_name, ios::in);
 
-	//std::cout << "Open: " << in_file_name << "-> " << in_file.is_open()<< endl;;
 	in_file.get(id);
 	while (!in_file.eof())
 	{
@@ -114,11 +103,9 @@ void huffman::create_pq()
 			in_file.get(id);
 			continue;
 		}
-		//std::cout << "Read: " << id << "-->" << (int)id << endl;;
 		node_array[id]->freq++;
 		in_file.get(id);
 	}
-	//std::cout << "create_pq filled freq table" << endl;;
 	in_file.close();
 	for (int i = 0; i < 256; i++)
 	{
@@ -133,7 +120,7 @@ void huffman::create_huffman_tree()
 {
 	priority_queue<node_ptr, vector<node_ptr>, compare> temp(pq);
 	while (temp.size() > 1)
-	{//create the huffman tree with highest frequecy characher being leaf from bottom to top
+	{
 		root = new huffman_node;
 		root->id = temp.top()->id;
 		root->freq = 0;
@@ -151,8 +138,6 @@ void huffman::create_huffman_tree()
 void huffman::calculate_huffman_codes()
 {
 	traverse(root, "");
-	//std::cout << "calculate_huffman_codes done" << endl;
-
 }
 
 void huffman::coding_save()
@@ -161,7 +146,7 @@ void huffman::coding_save()
 	out_file.open(out_file_name, ios::out | ios::binary);
 	string in = "", s = "";
 	//print2D(root);
-	in += (char)pq.size();																	//the first byte saves the size of the priority queue
+	in += (char)pq.size();
 	priority_queue<node_ptr, vector<node_ptr>, compare> toMap(pq);
 	std::unordered_map<char, node_ptr> letters;
 
@@ -175,39 +160,34 @@ void huffman::coding_save()
 
 	while (!temp.empty())
 	{
-		//get all characters and their huffman codes for output
 		node_ptr current = temp.top();
 		in += current->id;
-		s.assign(31 - current->code.size(), '0'); 											//set the codes with a fixed 127-bit string form[000бнбн1 + real code]
-		s += '1';																			//'1' indicates the start of huffman code
+		s.assign(31 - current->code.size(), '0'); 										
+		s += '1';
 		s.append(current->code);
-		//cout << "Check1" << "||" << s << endl;
 		in += (char)binary_to_decimal(s.substr(0, 8));
-		//cout << "Check1.5" << "||" << s << endl;
 		for (int i = 0; i < 3; i++)
-		{//cut into 8-bit binary codes that can convert into saving char needed for binary file
+		{
 			s = s.substr(8);
 			in += (char)binary_to_decimal(s.substr(0, 8));
 		}
 		temp.pop();
-		//cout << "Check2" << "||" << s << endl;
 
 	}
 	s.clear();
 
 	in_file.get(id);
 	while (!in_file.eof())
-	{//get the huffman code
+	{
 		if (id < 0) {
 			in_file.get(id);
 			continue;
 		}
 		s += letters[id]->code;
 		while (s.size() > 8)
-		{//cut into 8-bit binary codes that can convert into saving char needed for binary file
+		{
 			in += (char)binary_to_decimal(s.substr(0, 8));
-			s = s.substr(8);
-			
+			s = s.substr(8);	
 		}
 		in_file.get(id);
 		
@@ -216,10 +196,10 @@ void huffman::coding_save()
 
 	int count = 8 - s.size();
 	if (s.size() < 8)
-	{//append number of 'count' '0' to the last few codes to create the last byte of text
+	{
 		s.append(count, '0');
 	}
-	in += (char)binary_to_decimal(s);															//save number of 'count' at last
+	in += (char)binary_to_decimal(s);														
 	in += (char)count;
 
 	out_file.write(in.c_str(), in.size());
@@ -230,35 +210,35 @@ void huffman::coding_save()
 void huffman::recreate_huffman_tree()
 {
 	in_file.open(in_file_name, ios::in | ios::binary);
-	unsigned char size;																			//unsigned char to get number of node of hufmman tree
+	unsigned char size;																			
 	in_file.read(reinterpret_cast<char*>(&size), 1);
 	root = new huffman_node;
 	for (int i = 0; i < size; i++)
 	{
 		char r_id;
-		unsigned char h_code_c[4];																//16 unsigned char's to obtain the binary code
+		unsigned char h_code_c[4];																
 		in_file.read(&r_id, 1);     
 		in_file.read(reinterpret_cast<char*>(h_code_c), 4);
 		
 		string h_code_s = "";
 		for (int i = 0; i < 4; i++)
-		{										//obtain the oringinal 128-bit binary string
+		{										
 			h_code_s += decimal_to_binary(h_code_c[i]);
 		}
 		int j = 0;
 		while (h_code_s[j] == '0')
-		{//delete the added '000бнбн1' to get the real huffman code
+		{
 			j++;
 		}
 		h_code_s = h_code_s.substr(j + 1);
 		build_tree(h_code_s, r_id);
 	}
-	//print2D(root);
+	
 	in_file.close();
 }
 
 void huffman::build_tree(string& path, char r_id)
-{//build a new branch according to the inpue code and ignore the already existed branches
+{
 	node_ptr current = root;
 	for (int i = 0; i < path.size(); i++)
 	{
@@ -280,31 +260,29 @@ void huffman::build_tree(string& path, char r_id)
 
 void huffman::decoding_save()
 {
-	//cout << "Decoding saved root is " << root->id << endl;
 	in_file.open(in_file_name, ios::in | ios::binary);
 	out_file.open(out_file_name, ios::out);
-	unsigned char size;																		//get the size of huffman tree
+
+	unsigned char size;																		
 	in_file.read(reinterpret_cast<char*>(&size), 1);
-	in_file.seekg(-1, ios::end);															//jump to the last one byte to get the number of '0' append to the string at last
+	in_file.seekg(-1, ios::end);															
 	char count0;
 	in_file.read(&count0, 1);
-	in_file.seekg(1 + 5 * size, ios::beg);													//jump to the position where text starts
-	//cout << "Check 20" << endl;
+	in_file.seekg(1 + 5 * size, ios::beg);			
+
 	vector<unsigned char> text;
 	unsigned char textseg;
 	in_file.read(reinterpret_cast<char*>(&textseg), 1);
 	while (!in_file.eof())
-	{//get the text byte by byte using unsigned char
+	{
 		text.push_back(textseg);
 		in_file.read(reinterpret_cast<char*>(&textseg), 1);
 	}
-	//cout << "Check 30" << endl;
+
 	node_ptr current = root;
-	/*if (root == NULL)
-		cout << "Failed root " << root->id<<endl;*/
 	string path;
 	for (int i = 0; i < text.size() - 1; i++)
-	{//translate the huffman code
+	{
 		path = decimal_to_binary(text[i]);
 		if (i == text.size() - 2)
 			path = path.substr(0, 8 - count0);
@@ -328,12 +306,7 @@ void huffman::decoding_save()
 
 double huffman::get_comp_rate(string orig_file, string comp_file)
 {
-	//cout << "Enter the original file, then the compressed one!" << endl;
-	
-	//cin >> orig_file;
 	out_file.open(orig_file, ios::in);
-	
-	//cin >> comp_file;
 	in_file.open(comp_file, ios::in | ios::binary);
 	
 	double orig_size = 0;
@@ -388,7 +361,7 @@ void huffman::coding_show()
 	in_file.open(in_file_name, ios::in);
 	string in = "", s = "";
 	//print2D(root);
-	in += (char)pq.size();																	//the first byte saves the size of the priority queue
+	in += (char)pq.size();															
 	priority_queue<node_ptr, vector<node_ptr>, compare> toMap(pq);
 	std::unordered_map<char, node_ptr> letters;
 
@@ -403,14 +376,14 @@ void huffman::coding_show()
 
 	in_file.get(id);
 	while (!in_file.eof())
-	{//get the huffman code
+	{
 		if (id < 0) {
 			in_file.get(id);
 			continue;
 		}
 		s += letters[id]->code;
 		while (s.size() > 8)
-		{//cut into 8-bit binary codes that can convert into saving char needed for binary file
+		{
 			cout << binary_to_decimal(s.substr(0, 8)) << " ";
 			s = s.substr(8);
 
@@ -420,10 +393,10 @@ void huffman::coding_show()
 
 	int count = 8 - s.size();
 	if (s.size() < 8)
-	{//append number of 'count' '0' to the last few codes to create the last byte of text
+	{
 		s.append(count, '0');
 	}
-	cout << binary_to_decimal(s.substr(0, 8)) << " ";														//save number of 'count' at last
+	cout << binary_to_decimal(s.substr(0, 8)) << " ";														
 	in_file.close();
 	
 }
--- HuffmanAlgorithm/huffman.h
+++ HuffmanAlgorithm/huffman.h
@@ -10,13 +10,13 @@ using namespace std;
 
 struct huffman_node
 {
-	char id;																				//character 
-	int freq;																				//frequency of the character
-	string code;																			//huffman code for the character
+	char id;																				
+	int freq;																				
+	string code;																			
 	huffman_node* left;
 	huffman_node* right;
 	huffman_node()
-	{//constructer
+	{
 		left = right = NULL;
 	}
 };
@@ -25,14 +25,14 @@ typedef huffman_node* node_ptr;
 class huffman
 {
 protected:
-	node_ptr node_array[256];																//array for 128characters in the Ascii Table
-	std::unordered_map<char, node_ptr> letters;      //map for characters in the Ascii Table
+	node_ptr node_array[256];																
+	std::unordered_map<char, node_ptr> letters;      
 	fstream in_file, out_file;
 	node_ptr child, parent, root;
 	char id;
 	string in_file_name, out_file_name;
 	class compare
-	{//a object funtion to set comparing rule of priority queue
+	{
 	public:
 		bool operator()(const node_ptr& c1, const node_ptr& c2) const
 		{
@@ -42,12 +42,12 @@ protected:
 			return c1->freq > c2->freq;
 		}
 	};
-	priority_queue<node_ptr, vector<node_ptr>, compare> pq;									//priority queue of frequency from high to low
+	priority_queue<node_ptr, vector<node_ptr>, compare> pq;									
 	void create_node_array();
-	void traverse(node_ptr, string);														//traverse the huffman tree and get huffman code for a character
-	int binary_to_decimal(const string&);															//convert a 8-bit 0/1 string of binary code to a decimal integer 
-	string decimal_to_binary(int);															//convert a decimal integer to a 8-bit 0/1 string of binary code
-	inline void build_tree(string&, char);													//build the huffman tree according to information from file 
+	void traverse(node_ptr, string);														
+	int binary_to_decimal(const string&);															 
+	string decimal_to_binary(int);															
+	inline void build_tree(string&, char);													
 
 public:
 	huffman(string, string);
